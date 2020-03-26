04a087ecc35f835db219e97c2754496168084e99
Petr Samoilov <samoilov@aheadworks.com>
2020-03-26 14:48:54 +0300

    Check if ssh files exist

--- 2.3.2/7.2/assets/inject-ssh-keys.sh
+++ 2.3.2/7.2/assets/inject-ssh-keys.sh
@@ -6,6 +6,7 @@ SSH_DIR=${SSH_DIR:-/.ssh}
 if [[ ! -z ${SSH_DIR} ]] && [[ -d ${SSH_DIR} ]]; then
   cp -R $SSH_DIR $HOME/.ssh
   chmod 700 $HOME/.ssh
-  chmod -f 644 $HOME/.ssh/id_rsa.pub
-  chmod -f 600 $HOME/.ssh/id_rsa
+
+  test -f $HOME/.ssh/id_rsa.pub && chmod -f 644 $HOME/.ssh/id_rsa.pub
+  test -f $HOME/.ssh/id_rsa && chmod -f 600 $HOME/.ssh/id_rsa
 fi
